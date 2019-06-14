aade8fe26ff7969c58eba12597ce97bd2cc4e7de
Nishant Shaligram <nishant.shaligram@gmail.com>
2019-06-15 01:19:58 +0530

    Search form template created
    search form template created

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <div class="form-group">
+        <label class="screen-reader-text" for="s">Search for:</label>
+        <input class="form-control" type="text" value="" name="s" id="s">
+        <button class="btn btn-primary" type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
+    </div>
+</form>
